ddfc18bbd0db3385a10c72c1f0e7af69c0f4c26e
Guillaume TALBOT <talbotgui@gmail.com>
2024-10-16 20:42:27 +0200

    RGAA

--- projects/maclasse/src/app/div-contenu/div-contenu.component.ts
+++ projects/maclasse/src/app/div-contenu/div-contenu.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { tap } from 'rxjs';
 import { AbstractComponent } from '../directives/abstract.component';
 import { ContexteService } from '../service/contexte-service';
@@ -23,16 +23,39 @@ export class DivContenuComponent extends AbstractComponent implements OnInit {
   /** Flag indiquant que le menu peut être complètement affiché car les données sont chargées */
   public donneesDeClasseChargee = false;
 
+  /** Route active à un instant T */
+  public routeActive: string | undefined;
+
   /** Constructeur pour injection des dépendances. */
   public constructor(private contexteService: ContexteService, private router: Router) { super(); }
 
   /** Au chargement du composant */
   public ngOnInit(): void {
     // Si des données sont chargées, 
-    const sub = this.contexteService.obtenirUnObservableDuChargementDesDonneesDeClasse().pipe(
+    const sub1 = this.contexteService.obtenirUnObservableDuChargementDesDonneesDeClasse().pipe(
       //l'état est conservé
       tap(donnees => this.donneesDeClasseChargee = !!donnees)
     ).subscribe();
-    super.declarerSouscription(sub);
+    super.declarerSouscription(sub1);
+
+
+    // Mise en place d'une écoute des évènements du ROUTER d'Angular
+    const sub2 = this.router.events.pipe(
+      tap(e => {
+        // Si l'évènement est le dernier de la série liée à un changement de page dans le routeur
+        // @see https://medium.com/@gurunadhpukkalla/router-events-in-angular-3112a3968660
+        if (e instanceof NavigationEnd) {
+
+          // Récupération de l'URI sans les paramètres
+          let uri = (e as NavigationEnd).urlAfterRedirects;
+          const indexParam = uri.indexOf('?');
+          if (indexParam !== -1) {
+            uri = uri.substring(0, indexParam);
+          }
+          this.routeActive = uri;
+        }
+      })
+    ).subscribe();
+    super.declarerSouscription(sub2);
   }
 }
