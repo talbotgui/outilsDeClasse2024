ff704942076e100bf078c33213ab3f1f33602b48
Guillaume TALBOT <talbotgui@gmail.com>
2024-10-16 20:42:08 +0200

    Pour afficher une aide contextuelle

--- projects/maclasse/src/app/div-aide/div-aide.component.ts
+++ projects/maclasse/src/app/div-aide/div-aide.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { tap } from 'rxjs';
+import { AbstractComponent } from '../directives/abstract.component';
+import { ContexteService } from '../service/contexte-service';
 
 @Component({
     selector: '[div-aide]', templateUrl: './div-aide.component.html',
-    standalone: true, imports: []
+    standalone: true, imports: [MatIconModule]
 })
-export class DivAideComponent {
+export class DivAideComponent extends AbstractComponent implements OnInit {
+
+    /** Route active à un instant T */
+    public routeActive: string | undefined;
+
+    /** Flag sur le chargement des données de classe */
+    public donneesChargees = false;
+
+    /** Constructeur pour injection des dépendances. */
+    public constructor(private router: Router, private contexteService: ContexteService) { super(); }
+
+    /** Au chargement du composant */
+    public ngOnInit(): void {
+        // Mise en place d'une écoute des évènements du ROUTER d'Angular
+        const sub = this.router.events.pipe(
+            tap(this.traiterEvenement)
+        ).subscribe();
+        super.declarerSouscription(sub);
+
+        // Ecoute du chargement des données
+        const sub2 = this.contexteService.obtenirUnObservableDuChargementDesDonneesDeClasse().pipe(
+            tap(donnees => this.donneesChargees = !!donnees)
+        ).subscribe();
+        super.declarerSouscription(sub2);
+    }
+
+    /** Traitement de chaque EVENT envoyé par le ROUTER d'Angular */
+    private traiterEvenement(e: Event) {
+        // Si l'évènement est le dernier de la série liée à un changement de page dans le routeur
+        // @see https://medium.com/@gurunadhpukkalla/router-events-in-angular-3112a3968660
+        if (e instanceof NavigationEnd) {
+            this.routeActive = (e as NavigationEnd).urlAfterRedirects;
+        }
+    }
 }
